079d0e8def3321006706c574fef8f1afeaeb25a9
hwan kim <kimlove2324@gmail.com>
2026-05-14 18:05:01 +0900

    fix: 중복함수 제거

--- Assets/Scripts/CafeKioskOptionPopup.cs
+++ Assets/Scripts/CafeKioskOptionPopup.cs
@@ -198,14 +198,4 @@ public sealed class CafeKioskOptionPopup
     {
         CafeKioskUIUtility.SetSelectedInGroup(selectedButton, smallButton, regularButton, largeButton);
     }
-
-    private void SelectTemperatureButton(Button selectedButton)
-    {
-        CafeKioskUIUtility.SetSelectedInGroup(selectedButton, iceButton, hotButton);
-    }
-
-    private void SelectSizeButton(Button selectedButton)
-    {
-        CafeKioskUIUtility.SetSelectedInGroup(selectedButton, smallButton, regularButton, largeButton);
-    }
 }
